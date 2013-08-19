91705527201688c42a919f5274144d3aeb827050
Aaron Tracy <atracy@gmail.com>
2013-08-19 12:05:49 -0600

    added models and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,39 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
+    #view template is rendered
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit!)
+    #memorize this pattern for creating an item
+    if @post.save
+      flash[:notice] = "You created a new post!"
+      redirect_to posts_path
+    else
+      #handle validations
+      render :new
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
 
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :posts
-  belongs_to :user
+  belongs_to :post
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 end
